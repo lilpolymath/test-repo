2bd71bca5d57fb4b499c87da85d47bb77f12c111
lilpolymath <ayofavour7@gmail.com>
2020-07-12 22:24:13 +0100

    Updates

---
+++ precache-manifest.3cbc2116fd1de467859c62274e7d01b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7929fe8a56658b61da3462f153665a72",
+    "url": "/test-repo/index.html"
+  },
+  {
+    "revision": "6810201520bf6c6e6b3d",
+    "url": "/test-repo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d095fa87bda6b4ae3fbd",
+    "url": "/test-repo/static/js/2.a27f3f60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-repo/static/js/2.a27f3f60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6810201520bf6c6e6b3d",
+    "url": "/test-repo/static/js/main.ea6274b4.chunk.js"
+  },
+  {
+    "revision": "14c07ac17d04facfbfcb",
+    "url": "/test-repo/static/js/runtime-main.16f259bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-repo/static/media/logo.5d5d9eef.svg"
+  }
+]);
